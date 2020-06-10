3c053cfb10813a72d6c23976cf6a3c26db5b67ec
levanhuan99 <61698248+levanhuan99@users.noreply.github.com>
2020-06-10 14:10:22 +0700

    dang lam tiep phan gio hang

---
+++ src/model/Cart.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+
+    List<CartItem> cartItemList=new ArrayList<>();
+
+    public void addItem(CartItem item){
+        for (CartItem newItem:cartItemList){
+            int id=newItem.getProduct().getId();
+            if (id==item.getProduct().getId()){
+                increaseQuantityByProductID(id);
+            }
+        }
+    }
+
+    private void increaseQuantityByProductID(int id) {
+        for (CartItem item: cartItemList){
+            if (id==item.getProduct().getId()){
+                item.setQuantity(item.getQuantity()+1);
+            }
+        }
+    }
+    public List<CartItem> getCartItemList(){
+        return this.cartItemList;
+    }
+}
